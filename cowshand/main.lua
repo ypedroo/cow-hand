ca224c85930b4e3f7764bea1798cfa5c9f08de95
ypedroo <pedromota99@gmail.com>
2018-02-25 23:31:24 -0300

    adding the floor

--- cowshand/main.lua
+++ cowshand/main.lua
@@ -1,7 +1,13 @@
+display.setStatusBar(display.HiddenStatusBar)
 local physics = require( "physics" )
 physics.start()
-physics.setGravity( 0, 0 )
+--physics.setGravity( 0, 0 )
 
+
+local ground = display.newImageRect( "ui/ground.png", 12000, 30)
+ground.x = display.contentCenterX -600
+ground.y =  display.contentHeight -100 
+physics.addBody(ground, "static")
 
 local sky = display.newImageRect("ui/sky.png", 1500, 750)
 sky.x = display.contentCenterX
@@ -15,15 +21,24 @@ bg.y = display.contentCenterY
 
 local cow = display.newImageRect( "ui/cow.png", 180, 200 )
 cow.x = display.contentCenterX -600
-cow.y = display.contentHeight -80
+cow.y = display.contentHeight -120
+physics.addBody(cow, "dynamic", { density = 0, friction = 0, bounce = 0, gravity = 0 })
+        --cow.isFixedRotation=true	
+        
+local lives = 6
+local score = 0
+local died = false
 
+local function onTouch(event)
+    if(event.phase == "began") then
+        cow:setLinarVelocity(0, -200)
+    end
+end
 
-local floor = display.newImageRect( "ui/floor.png", 300, 50 )
-floor.x = display.contentCenterX
-floor.y = display.contentHeight 
+Runtime:addEventListener("touch", onTouch)
 
 -- isso aqui tem que virar image sheet
---ocal golpe = display.newImageRect( "golpe.png", 120, 150 )
+--local golpe = display.newImageRect( "golpe.png", 120, 150 )
 --golpe.x = display.contentCenterX 
 --golpe.y = display.contentHeight -100
 --physics.addBody( golpe, { radius=30, isSensor=true } )
@@ -36,23 +51,3 @@ floor.y = display.contentHeight
 --physics.addBody( gorgoyle, { radius=30, isSensor=true } )
 --gorgoyle.name = "minion"
 
-local lives = 6
-local score = 0
-local died = false
-
-local physics = require( "physics" )
-physics.start()
-
---physics.addBody( platform, "static" )
-physics.addBody(cow, "dynamic", { density = 0, friction = 0, bounce = 0, gravity = 0 })
-		cow.isFixedRotation=true	
-
-        local function onTouch(event)
-            if(event.phase == "ended") then
-             transition.to( cow, {x=event.x, y=event.y})
-            end
-          end
-          
-          Runtime: addEventListener( "onTouch", onTouch )
-
-Runtime:addEventListener("touch", cow)
